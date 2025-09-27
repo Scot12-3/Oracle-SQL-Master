8219fbb3616fce82856bd90f39b872daee03ff45
Scot12-3 <smessler999@gmail.com>
2025-09-27 16:54:31 -0400

    part 4

--- SQL Code/Character+Functions+-+Part+3+(TRIM,+LTRIM,+RTRIM+Functions)(Code+Samples).sql
+++ SQL Code/Character+Functions+-+Part+3+(TRIM,+LTRIM,+RTRIM+Functions)(Code+Samples).sql
@@ -27,3 +27,15 @@ SELECT RTRIM ('my Name is Adammmm', 'my') r_trimmed_text from dual;
 SELECT LTRIM ('www.yourwebsite.com', 'w.') l_trimmed_text from dual;
 SELECT RTRIM(LTRIM('www.yourwebsitename.com', 'w.'),'.com') trimmed_text from dual;
 SELECT ltrim('1237982434www.yourwebsitename.com', '0123456789') trimmed_text from dual;
+
+--PART 4:
+SELECT first_name, REPLACE(first_name,'a') rpl FROM employees;
+SELECT first_name, REPLACE(first_name,'a','-') rpl FROM employees;
+SELECT first_name, REPLACE(first_name,'le','-') rpl FROM employees;
+SELECT first_name, REPLACE(first_name,'und','-') rpl FROM employees;
+SELECT first_name, lpad(first_name,10,'*') pad FROM employees;
+SELECT first_name, rpad(first_name,10,'*') pad FROM employees;
+SELECT first_name, rpad(first_name,6,'*') pad FROM employees;
+SELECT first_name, lpad(first_name,6,'*') pad FROM employees;
+SELECT first_name, lpad('My name is ',20,'-') pad FROM employees;
+SELECT first_name, lpad('My name is '||last_name ,20,'-') pad FROM employees;
