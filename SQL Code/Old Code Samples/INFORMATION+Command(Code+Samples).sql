a448bcb820197944679a50bc5a0d2bb9609da20f
Scot12-3 <smessler999@gmail.com>
2025-10-02 19:21:15 -0400

    New Folder

---
+++ SQL Code/Old Code Samples/INFORMATION+Command(Code+Samples).sql
@@ -0,0 +1,7 @@
+DESC employees;
+INFORMATION employees;
+INFO departments;
+INFO+ employees;        
+INFORMATION+ employees;
+-- Note: The above commands (INFO, INFORMATION) are specific to certain Oracle tools like SQL*Plus and may not work in all environments.
+-- They provide metadata about the specified table, such as column names, data types, and constraints.  
